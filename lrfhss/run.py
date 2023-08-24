6f9417b4e841ec588bf0b5212ea689c0cf65d587
Jean Michel de Souza Sant'Ana <jeanssant@gmail.com>
2023-08-24 11:41:37 +0300

    Update run.py to account new inheritance bases

--- lrfhss/run.py
+++ lrfhss/run.py
@@ -1,25 +1,29 @@
 from lrfhss.lrfhss_core import *
+from lrfhss.acrda import BaseACRDA
+from lrfhss.settings import Settings
 import simpy
 
 def run_sim(settings: Settings, seed=0):
     random.seed(seed)
     np.random.seed(seed)
-    bs = Base(settings.obw, settings.sic, settings.window_size, settings.window_step, settings.time_on_air, settings.threshold)
     env = simpy.Environment()
+    if settings.base=='acrda':
+        bs = BaseACRDA(settings.obw, settings.window_size, settings.window_step, settings.time_on_air, settings.threshold)
+        env.process(bs.sic_window(env))
+    else:
+        bs = Base(settings.obw, settings.threshold)
+    
     nodes = []
-
     for i in range(settings.number_nodes):
         node = Node(settings.average_interval, settings.obw, settings.headers, settings.payloads, settings.header_duration, settings.payload_duration)
         bs.add_node(node.id)
         nodes.append(node)
-        env.process(transmit(env, bs, node, settings.sic, settings.transceiver_wait))
-    if settings.sic:
-        env.process(bs.sic_window(env))
+        env.process(transmit(env, bs, node, settings.transceiver_wait))
     # start simulation
     env.run(until=settings.simulation_time)
 
+    # after simulation
     success = sum(bs.packets_received.values())
-
     transmitted = sum(n.transmitted for n in nodes) 
 
     return success/transmitted
