494413969d130f0151f1e573a969408e88ba5a9e
Jean Michel de Souza Sant'Ana <jeanssant@gmail.com>
2024-02-02 11:05:31 +0200

    typo on constant traffic initial value

--- lrfhss/traffic.py
+++ lrfhss/traffic.py
@@ -15,6 +15,6 @@ def uniform_traffic(self):
 def constant_traffic(self):
     # First transmissions is random, devices are not initiated at the same time
     if self.transmitted == 0:
-        return random.uniform(0,self.traffic_param['constant_interval'])
+        return random.uniform(0,2*self.traffic_param['constant_interval'])
     else:
         return max(0, self.traffic_param['constant_interval'] + random.gauss(0,self.traffic_param['standard_deviation']))
