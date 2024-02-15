9381de4fabf94c890639b4de7bddb183292e96e7
Jean Michel de Souza Sant'Ana <jeanssant@gmail.com>
2024-02-15 13:46:19 +0200

    Add 2-state markov chain traffic model

--- lrfhss/traffic.py
+++ lrfhss/traffic.py
@@ -18,3 +18,22 @@ def constant_traffic(self):
         return random.uniform(0,2*self.traffic_param['constant_interval'])
     else:
         return max(0, self.traffic_param['constant_interval'] + random.gauss(0,self.traffic_param['standard_deviation']))
+    
+
+def two_state_markovian_traffic(self):
+    discrete_time = 1
+    try:
+        state = self.state
+    except AttributeError:
+        state = 0
+
+    if random.random() >= self.traffic_param['transition_matrix'][state][0]:
+        return max(0,discrete_time * self.traffic_param['markov_time'] + random.gauss(0,1))
+
+    discrete_time+=1
+    transition_probability = self.traffic_param['transition_matrix'][0][0]
+    while random.random() < transition_probability:
+        discrete_time+=1
+    
+    self.state=1
+    return max(0,discrete_time * self.traffic_param['markov_time'] + random.gauss(0,1))
