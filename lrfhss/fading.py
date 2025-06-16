228fcd1ad92bed1296a204e3ff0d2a2e8c419b69
Misael Costa <misaelrdacosta@gmail.com>
2025-06-16 08:06:24 -0300

    versao tcc

--- lrfhss/fading.py
+++ lrfhss/fading.py
@@ -9,13 +9,11 @@ from scipy.stats import rayleigh, rice, nakagami
 class Rayleigh_Fading(Fading):
     def __init__(self, fading_param):
         super().__init__(fading_param)
-        #TODO: Implementar
         if not 'scale' in self.fading_param:
             warnings.warn(f'traffic_param scale key missing for Rayleigh_Fading. Using with scale=1 as default')
             self.fading_param['scale'] = 1.0
 
     def fading_function(self):
-        #TODO: Implementar
         scale = self.fading_param['scale']
         return rayleigh.rvs(scale=scale)        
     
@@ -23,22 +21,18 @@ class Rayleigh_Fading(Fading):
 class Rician_Fading(Fading):
     def __init__(self, fading_param):
         super().__init__(fading_param)
-        #TODO: Implementar
         if not 'k' in self.fading_param:
             warnings.warn(f'traffic_param k key missing for Rician_Fading. Using with k=3 as default')
             self.fading_param['k'] = 3
         
     def fading_function(self):
-        #TODO: Implementar
         K = self.fading_param['k']
-
         return rice.rvs(np.sqrt(2*K), scale= 1/np.sqrt(2*(K+1)))
         
 ## Nakagami-m fading
 class Nakagami_M_Fading(Fading):
     def __init__(self, fading_param):
         super().__init__(fading_param)
-        #TODO: Implementar
         if not 'm' in self.fading_param:
             warnings.warn(f'traffic_param m key missing for Nakagami_M_Fading. Using with m=1 as default')
             self.fading_param['m'] = 1.0 # para m=1, equivale ao Rayleigh
@@ -47,7 +41,6 @@ class Nakagami_M_Fading(Fading):
             self.fading_param['omega'] = 1.0  # Energia média do sinal
 
     def fading_function(self):
-        #TODO: Implementar
         m = self.fading_param['m']
         omega = self.fading_param['omega']
         return nakagami.rvs(m, scale=omega)
